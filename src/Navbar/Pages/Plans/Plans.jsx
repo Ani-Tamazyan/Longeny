e9f04d023fa9c3f655beb347338f6d399c4db4f7
Ani Tamazyan <anitamazyan2000@gmail.com>
2023-11-27 23:31:34 +0400

    first commit

--- src/Navbar/Pages/Plans/Plans.jsx
+++ src/Navbar/Pages/Plans/Plans.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./Plans.css"
 import countryside from "./images/walk-countryside.png";
@@ -11,6 +11,18 @@ import drop from "./images/drop.png"
 import zig from "./images/zig.png"
 
 function Plans() {
+    const [openSection, setOpenSection] = useState(null);
+    
+  const handleDropClick = (section) => {
+    if (openSection === section) {
+      
+      setOpenSection(null);
+    } else {
+      
+      setOpenSection(section);
+    }
+  };
+
     return (
         <>
         <div className='part1'>
@@ -219,112 +231,104 @@ function Plans() {
             <p>Here’s What you Get with 360ME</p>
 
             <div className='part5_info'>
-                <div className='part5_info_header'>
-                    <h3>Comprehensive Biomarker Analysis</h3>
-                    <div className='part5_info_header_img'>
-                        <img src={drop} />
-                    </div>
-                </div>              
-                {/* <div className='part5_info1_hiden'>
-
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-
-                </div> */}
+          <div className='part5_info_header'>
+            <h3>Comprehensive Biomarker Analysis</h3>
+            <div className='part5_info_header_img' onClick={() => handleDropClick('biomarker')}>
+              <img src={drop} alt="drop" />
             </div>
+          </div>
+
+          {openSection === 'biomarker' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
+        </div>
 
             <div className='part5_info'>
                 <div className='part5_info_header'>
                     <h3>Full-Body MRI Scan</h3>
-                    <div className='part5_info_header_img'>
+                    <div className='part5_info_header_img' onClick={() => handleDropClick('mri')}>
                         <img src={drop} />
                     </div>
                 </div>                
-                {/* <div className='part5_info2_hiden'>
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-                </div> */}
+                {openSection === 'mri' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
             </div>
 
             <div className='part5_info'>
                 <div className='part5_info_header'>
                     <h3>Advanced Cardiac Risk Assessment</h3>
-                    <div className='part5_info_header_img'>
+                    <div className='part5_info_header_img'  onClick={() => handleDropClick('assessment')}>
                         <img src={drop} />
                     </div>
                 </div>  
                 
-                {/* <div className='part5_info3_hiden'>
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-                </div> */}
+                {openSection === 'assessment' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
             </div>
 
             <div className='part5_info'>
                 <div className='part5_info_header'>
                     <h3>Early Disease Screening with Genomic Tests</h3>
-                    <div className='part5_info_header_img'>
+                    <div className='part5_info_header_img' onClick={() => handleDropClick('screening')}>
                         <img src={drop} />
                     </div>
                 </div>  
                 
-                {/* <div className='part5_info4_hiden'>
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-                </div> */}
+                {openSection === 'screening' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
             </div>
 
             <div className='part5_info'>
                 <div className='part5_info_header'>
                     <h3>Gut Microbiome Analysis</h3>
-                    <div className='part5_info_header_img'>
+                    <div className='part5_info_header_img' onClick={() => handleDropClick('analysis')}>
                         <img src={drop} />
                     </div>
                 </div>  
                 
-                {/* <div className='part5_info5_hiden'>
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-                </div> */}
+                {openSection === 'analysis' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
             </div>
 
             <div className='part5_info'>
                 <div className='part5_info_header'>
                     <h3>DEXA Scan (Bone Density Scan)</h3>
-                    <div className='part5_info_header_img'>
+                    <div className='part5_info_header_img' onClick={() => handleDropClick('dexa')}>
                         <img src={drop} />
                     </div>
                 </div>  
                 
-                {/* <div className='part5_info6_hiden'>
-                    <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs. We analyze a wide range of biomarkers, 
-                        including hormone levels, inflammation markers, nutrient levels, and more.</p>
-                    <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases, including heart disease, metabolic disorders,
-                         and cancers and provide personalized recommendations to optimize your health.</p>
-                    
-                    <button className='bookFreeCall'>Book my call now!</button>
-                </div> */}
+                {openSection === 'dexa' && (
+            <div className='part5_info_hiden'>
+              <p>Our biomarker analysis is designed to provide a detailed evaluation of your body's unique needs.</p>
+              <p>By measuring these biomarkers, we can identify potential health risks for chronic diseases.</p>
+              <button className='bookFreeCall'>Book my call now!</button>
+            </div>
+          )}
             </div>
 
         </div>
